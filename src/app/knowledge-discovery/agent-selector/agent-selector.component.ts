bea4e2ea4bc97b180ff3c293369bd70d7d1257c1
Sherry Mathews <sherry.mathews@hyland.com>
2025-08-21 20:52:19 -0500

    Class Seven Materials

--- src/app/knowledge-discovery/agent-selector/agent-selector.component.ts
+++ src/app/knowledge-discovery/agent-selector/agent-selector.component.ts
@@ -1,12 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { AppConfigService } from '@alfresco/adf-core';
+import { KnowledgeDiscoveryService } from '../services/knowledge-discovery.service';
+
+export interface AgentData {
+    id: string;
+    name: string;
+    description: string;
+    type: string;
+    status: string;
+    createdOn: string;
+    isGlobal: string;
+}
 
 @Component({
-  selector: 'app-agent-selector',
-  standalone: true,
-  imports: [],
-  templateUrl: './agent-selector.component.html',
-  styleUrl: './agent-selector.component.scss'
+    selector: 'agent-selector',
+    templateUrl: './agent-selector.component.html',
+    styleUrls: ['./agent-selector.component.scss'],
+    providers: [KnowledgeDiscoveryService]
 })
 export class AgentSelectorComponent {
 
+    @Output() agentIsSelected = new EventEmitter<any>();
+
+    showPopup = false;
+    tempSelectedAgentID: string | null = null;
+    agents: AgentData[] = [];
+    selectedAgentID: string = '';
+    selectedAgent: AgentData | null = null;
+    loading: boolean = false;
+    error: string = '';
+    agentId: string = '';
+
+
+    constructor(
+        private appConfig: AppConfigService,
+        private kdService: KnowledgeDiscoveryService
+    ) {
+        
+    }
+
+    ngOnInit() {
+        this.loadAgents();
+    }
+
+    loadAgents() {
+        this.loading = true;
+        this.error = '';
+        this.kdService.loadAgents().subscribe({
+            next: (data) => {
+                console.log('API Data:', data)  ; // Log the data received from the API
+                this.agents = data;
+                this.loading = false;
+            },
+            error: (err) => {
+                this.error = 'Failed to load agents from API';
+                this.loading = false;
+                console.error('API Error:', err); // Log the error received from the API
+            }
+        });
+
+    }
+
+    openPopup(): void {
+        this.showPopup = true;
+        this.tempSelectedAgentID = '';
+    }
+
+    closePopup(): void {
+        this.showPopup = false;
+        this.tempSelectedAgentID = '';
+    }
+
+    confirmSelection(): void {
+        this.agentIsSelected.emit(this.getSelectedAgentData());
+        if (this.tempSelectedAgentID) {
+            this.selectedAgentID = this.tempSelectedAgentID;
+            this.closePopup();
+        }
+    }
+
+    getSelectedAgentData(): AgentData | undefined {
+        return this.agents.find(agent => agent.id === this.tempSelectedAgentID);
+    }
 }
